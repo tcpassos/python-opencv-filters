ceb04e94b1f3b1205bf3a115999bfa4a179256d4
tcpassos <tcpassos99@gmail.com>
2023-06-26 10:14:58 -0300

    Organização do código

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ class OpenCVFilters(QtWidgets.QWidget):
 
         # Configura o classificador de rostos
         cascPath = "haarcascade_frontalface_default.xml"
-        self.faceCascade = cv2.CascadeClassifier(cascPath)
+        self.face_cascade = cv2.CascadeClassifier(cascPath)
 
         # Inicia valores para filtros
         self.rvalue = 0
@@ -126,16 +126,8 @@ class OpenCVFilters(QtWidgets.QWidget):
         self.control_groupbox.setLayout(self.control_layout)  
 
         # Esconde campos específico de determinados filtros
-        self.control_rlabel.hide()
-        self.control_rslider.hide()
-        self.control_glabel.hide()
-        self.control_gslider.hide()
-        self.control_blabel.hide()
-        self.control_bslider.hide()        
-        self.control_blabel.hide()
-        self.control_bslider.hide()   
-        self.control_limitlabel.hide()
-        self.control_limitslider.hide()
+        self.set_rgb_controls_visible(False)
+        self.set_threshold_controls_visible(False)
 
         self.load_face_sticker_button = QtWidgets.QPushButton("Carregar Máscara")
         self.remove_stickers_button = QtWidgets.QPushButton("Remover Adesivos")
@@ -157,12 +149,12 @@ class OpenCVFilters(QtWidgets.QWidget):
         self.control_limitslider.valueChanged.connect(self.set_limitvalue)
 
         # Define os callbacks dos botões
-        QtCore.QObject.connect(self.capture_file_button, QtCore.SIGNAL("clicked()"), self.capture_file)
-        QtCore.QObject.connect(self.camera_button, QtCore.SIGNAL("clicked()"), self.capture_camera)
-        QtCore.QObject.connect(self.apply_filter_button, QtCore.SIGNAL("clicked()"), self.apply_filter)
-        QtCore.QObject.connect(self.load_face_sticker_button, QtCore.SIGNAL("clicked()"), self.load_face_sticker)
-        QtCore.QObject.connect(self.remove_stickers_button, QtCore.SIGNAL("clicked()"), self.remove_stickers)
-        QtCore.QObject.connect(self.save_frame_button, QtCore.SIGNAL("clicked()"), self.save_frame)
+        QtCore.QObject.connect(self.capture_file_button, QtCore.SIGNAL("clicked()"), self.on_capture_file)
+        QtCore.QObject.connect(self.camera_button, QtCore.SIGNAL("clicked()"), self.on_capture_camera)
+        QtCore.QObject.connect(self.apply_filter_button, QtCore.SIGNAL("clicked()"), self.on_filter_change)
+        QtCore.QObject.connect(self.load_face_sticker_button, QtCore.SIGNAL("clicked()"), self.on_load_face_sticker)
+        QtCore.QObject.connect(self.remove_stickers_button, QtCore.SIGNAL("clicked()"), self.on_remove_stickers)
+        QtCore.QObject.connect(self.save_frame_button, QtCore.SIGNAL("clicked()"), self.on_save_frame)
 
         # Configura o callback de renderização do frame
         self.frame_timer.timeout.connect(self.display_video_stream)
@@ -179,7 +171,7 @@ class OpenCVFilters(QtWidgets.QWidget):
         self.main_layout.addWidget(self.save_frame_button, 5, 0, 1, 2)
         self.setLayout(self.main_layout)
 
-    def capture_file(self):
+    def on_capture_file(self):
         self.video_capture.release()
         path, _ = QtWidgets.QFileDialog.getOpenFileName(filter="Videos (*.mp4);;Imagens (*.png *.jpg *.jpeg)")
         if path.lower().endswith(('.png', '.jpg', '.jpeg')):
@@ -189,7 +181,7 @@ class OpenCVFilters(QtWidgets.QWidget):
             self.capture_type = CaptureType.video
             self.video_capture.open(path)
 
-    def capture_camera(self):
+    def on_capture_camera(self):
         self.video_capture.release()
         self.video_capture.open(2)
         self.capture_type = CaptureType.camera
@@ -198,7 +190,7 @@ class OpenCVFilters(QtWidgets.QWidget):
         self.camera_capture.set(3, self.video_size.width())
         self.camera_capture.set(4, self.video_size.height())
     
-    def apply_filter(self):           
+    def on_filter_change(self):           
         self.selected_filter = self.filter_combo.currentText()        
         self.set_rgb_controls_visible(self.selected_filter == "Colorização")
         self.set_threshold_controls_visible(self.selected_filter == "Binarização")
@@ -227,7 +219,7 @@ class OpenCVFilters(QtWidgets.QWidget):
             self.control_limitlabel.hide()
             self.control_limitslider.hide()
 
-    def save_frame(self):
+    def on_save_frame(self):
         if self.frame is not None:
             current_frame = self.frame
             dialog = QtWidgets.QFileDialog()
@@ -238,15 +230,16 @@ class OpenCVFilters(QtWidgets.QWidget):
                 file_path = dialog.selectedFiles()[0]
                 cv2.imwrite(file_path, cv2.cvtColor(current_frame, cv2.COLOR_BGR2RGB))
 
+    # Quando for clicado no frame será aberto uma janela para a seleção do adesivo a ser adicionado nessa posição
     def mousePressEvent(self, event):
-        if event.button() == QtCore.Qt.LeftButton and self.valid_x(event.x()) and self.valid_y(event.y()):
-            path = QtWidgets.QFileDialog.getOpenFileName(filter="Stickers (*.png)", dir=".\Stickers")
-            if(path[0] != ''):
-                x = event.x()
-                y = event.y()
-                self.stickers.append(Sticker(x, y, path, path[0].split('/').pop()))
+        if event.button() == QtCore.Qt.LeftButton:
+            x = event.x()
+            y = event.y()
+            if x > 0 and x < self.video_size.width() and y > 0 and y < self.video_size.height():
+                path = QtWidgets.QFileDialog.getOpenFileName(filter="Stickers (*.png)", dir=".\Stickers")
+                if(path[0] != ''):
+                    self.stickers.append(Sticker(x, y, path, path[0].split('/').pop()))
             
-
     def set_bvalue(self, value):
         self.bvalue = value
         self.control_blabel.setText(f"B: {value}")
@@ -263,10 +256,44 @@ class OpenCVFilters(QtWidgets.QWidget):
         self.limitvalue = value              
         self.control_limitlabel.setText(f"Limiar: {value}")
 
+    # Abre a janela para seleção do adesivo de rosto
+    def on_load_face_sticker(self):
+        path = QtWidgets.QFileDialog.getOpenFileName(filter="Stickers (*.png)", dir="./Masks")
+        if(path[0] != ''):
+            self.face_sticker = Image.open(path[0])
+
+    # Remove todos os adesivos do frame
+    def on_remove_stickers(self):
+        self.face_sticker = None
+        self.stickers.clear()
+
     # ============================================================================================================
-    # Renderização do frame
+    # Processamento e renderização do frame
     # ============================================================================================================
     def display_video_stream(self):
+        if not self.read_frame():
+            return False
+        # Detecta os rostos no frame
+        faces = self.detect_faces()
+        # Aplica o filtro selecionado
+        self.apply_filter()
+        # Desenha o sticker do rosto
+        self.draw_face_sticker(faces)
+        # BGR -> RGB
+        self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
+        # Se for webcam, espelha a imagem
+        if self.capture_type == CaptureType.camera:
+            self.frame = cv2.flip(self.frame, 1)
+        # Se for vídeo/imagem, redimensiona encaixar na janela
+        else:
+            self.frame = cv2.resize(self.frame, (self.video_size.width(), self.video_size.height()), interpolation=cv2.INTER_AREA)
+        # Desenha os stickers
+        self.draw_stickers()
+        # Adiciona o frame processado pelo OpenCV na janela
+        image = QtGui.QImage(self.frame, self.video_size.width(), self.video_size.height(), self.video_size.width() * 3, QtGui.QImage.Format_RGB888)
+        self.frame_label.setPixmap(QtGui.QPixmap.fromImage(image))
+    
+    def read_frame(self):
         if self.capture_type == CaptureType.camera:
             ret, self.frame = self.camera_capture.read()
         elif self.capture_type == CaptureType.video:
@@ -274,24 +301,46 @@ class OpenCVFilters(QtWidgets.QWidget):
         elif self.capture_type == CaptureType.image:
             self.frame = self.image
             ret = self.frame is not None
-
-        if not ret:
-            return False
-                
-        showMask = False
-        if self.faceCascade is not None and showMask and self.face_sticker is not None:
+        return ret
+    
+    def detect_faces(self):
+        faces = []
+        if self.face_cascade is not None and self.face_sticker is not None:
             gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-
-            # Detecta os rostos no frame
-            faces = self.faceCascade.detectMultiScale(
+            faces = self.face_cascade.detectMultiScale(
                 gray,
                 scaleFactor=1.2,
                 minNeighbors=5,
                 minSize=(60, 50)
             )
-            showMask = True
+        return faces
 
-        # Aplica o filtro selecionado
+    def draw_face_sticker(self, faces):
+        if self.face_sticker is None:
+            return
+        for (x, y, w, h) in faces:
+            sticker = self.face_sticker.resize((int(w*4/3), int(h*4/3)))
+            frame_pil = Image.fromarray(self.frame)
+            frame_pil.paste(sticker, (int(x-(w/6)), int(y-(h/6))), sticker)
+            self.frame = np.array(frame_pil)
+
+    def draw_stickers(self):
+        for sticker in self.stickers:   
+            if(sticker.img.size[0] > sticker.img.size[1]):
+                x = 120
+                y = int(sticker.img.size[1]*x/sticker.img.size[0])
+            else:
+                y = 120
+                x = int(sticker.img.size[0]*y/sticker.img.size[1])
+            stc = sticker.img.resize((x, y))
+            frame_pil = Image.fromarray(self.frame)
+            try:
+                frame_pil.paste(stc, (sticker.x-int(x/2), sticker.y-int(y/2)), stc)
+            except:
+                frame_pil.paste(stc, (sticker.x-int(x/2), sticker.y-int(y/2)))
+            self.frame = np.array(frame_pil)
+
+    def apply_filter(self):
         channels = cv2.split(self.frame)
         if self.selected_filter == "Cinza":
             self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
@@ -318,73 +367,17 @@ class OpenCVFilters(QtWidgets.QWidget):
             l, self.frame = cv2.threshold(self.frame, self.limitvalue, 255, cv2.THRESH_BINARY)
         elif self.selected_filter == "Sepia":
             sepia_matrix = np.array([[0.272, 0.534, 0.131], [0.349, 0.686, 0.168], [0.393, 0.769, 0.189]])
-            self.frame = cv2.transform(self.frame, sepia_matrix)        
+            self.frame = cv2.transform(self.frame, sepia_matrix)
 
-        # Desenha o sticker do rosto
-        if showMask and self.face_sticker is not None:
-            for (x, y, w, h) in faces:
-                sticker = self.face_sticker.resize((int(w*4/3), int(h*4/3)))
-                frame_pil = Image.fromarray(self.frame)
-                frame_pil.paste(sticker, (int(x-(w/6)), int(y-(h/6))), sticker)
-                self.frame = np.array(frame_pil)
-        #else:
-            #for (x, y, w, h) in faces:
-                #cv2.rectangle(self.frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-        self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
-
-        image = QtGui.QImage(self.frame, self.video_size.width(), self.video_size.height(), self.video_size.width() * 3, QtGui.QImage.Format_RGB888)
-        self.frame_label.setPixmap(QtGui.QPixmap.fromImage(image))
-
-        if self.capture_type == CaptureType.camera:
-            self.frame = cv2.flip(self.frame, 1)
-        else:
-            self.frame = cv2.resize(self.frame, (self.video_size.width(), self.video_size.height()), interpolation=cv2.INTER_AREA)
-        
-        # Desenha os stickers
-        for sticker in self.stickers:   
-            if(sticker.img.size[0] > sticker.img.size[1]):
-                x = 120
-                y = int(sticker.img.size[1]*x/sticker.img.size[0])
-            else:
-                y = 120
-                x = int(sticker.img.size[0]*y/sticker.img.size[1])
-            stc = sticker.img.resize((x, y))
-            frame_pil = Image.fromarray(self.frame)
-            try:
-                frame_pil.paste(stc, (sticker.x-int(x/2), sticker.y-int(y/2)), stc)
-            except:
-                frame_pil.paste(stc, (sticker.x-int(x/2), sticker.y-int(y/2)))
-            self.frame = np.array(frame_pil)
-
-        image = QtGui.QImage(self.frame, self.video_size.width(), self.video_size.height(), self.video_size.width() * 3, QtGui.QImage.Format_RGB888)
-        self.frame_label.setPixmap(QtGui.QPixmap.fromImage(image))
-
-    def load_face_sticker(self):
-        path = QtWidgets.QFileDialog.getOpenFileName(filter="Stickers (*.png)", dir="./Masks")
-        self.face_sticker = Image.open(path[0])
-
-    def remove_stickers(self):
-        self.face_sticker = None
-        self.stickers.clear()
-
-    def valid_x(e, x):
-        return x > 0 and x < 640
-    
-    def valid_y(e, y):
-        return y > 0 and y < 480
-
-
-def close_win(self):
-    self.camera_capture.release()
-    self.video_capture.release()
-    cv2.destroyAllWindows()
-    self.close()
-
+    def close_win(self):
+        self.camera_capture.release()
+        self.video_capture.release()
+        cv2.destroyAllWindows()
+        self.close()
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     player = OpenCVFilters()
-    player.apply_filter()
+    player.on_filter_change()
     player.show()
     sys.exit(app.exec_())
